9676dfdb37ed6ca84d8559c670b421844e573c17
zzz-t <102299657+zzz-t@users.noreply.github.com>
2026-03-12 12:40:30 +0800

    Initialize web5

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,47 @@
+use tauri::menu::*;
+mod command;
+mod utils;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .menu(|handle| {
+            let menu = Menu::with_items(
+                handle,
+                &[
+                    #[cfg(target_os = "macos")]
+                    &Submenu::with_items(
+                        handle,
+                        "Edit",
+                        true,
+                        &[
+                            &PredefinedMenuItem::undo(handle, None)?,
+                            &PredefinedMenuItem::redo(handle, None)?,
+                            &PredefinedMenuItem::cut(handle, None)?,
+                            &PredefinedMenuItem::copy(handle, None)?,
+                            &PredefinedMenuItem::paste(handle, None)?,
+                            &PredefinedMenuItem::select_all(handle, None)?,
+                        ],
+                    )?,
+                ],
+            );
+            menu
+        })
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![
+            command::cmds::open_url,
+            command::cmds::run_command,
+            command::cmds::download_file,
+            command::cmds::get_exe_dir,
+            command::cmds::get_env_var,
+            command::cmds::find_port,
+        ])
+        .setup(|app| {
+            tauri::async_runtime::block_on(async move {
+                let _ = utils::init::resolve_setup(app).await;
+            });
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
